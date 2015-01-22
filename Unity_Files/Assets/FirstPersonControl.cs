15a2d0b60f03bd4d99195db75530bd0509808176
Charlie Yang <charlesy@stanford.edu>
2015-01-21 18:04:59 -0800

    added camera control, rotates kinda weird

---
+++ Unity_Files/Assets/FirstPersonControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonControl : MonoBehaviour {
+
+
+	public float xRotatePower = 2.0f;
+	public float yRotatePower = 2.0f;
+
+	public float xTranslatePower = 2.0f;
+	public float zTranslatePower = 2.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float xAxisValue = Input.GetAxis("Horizontal") * xTranslatePower;
+     	float zAxisValue = Input.GetAxis("Vertical") * zTranslatePower;
+
+     	float xRotateVal = Input.GetAxis("RotateHorizontal") * xRotatePower;
+     	float yRotateVal = - Input.GetAxis("RotateVertical") * yRotatePower;
+
+     	// if(Camera.current != null) {
+        	transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+        	transform.Rotate(new Vector3(yRotateVal, xRotateVal, 0.0f ));
+     	// }
+
+	}
+
+}

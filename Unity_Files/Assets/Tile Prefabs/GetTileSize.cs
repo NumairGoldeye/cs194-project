b951c07e538296d2d1f1951e724ad1c39be9f34c
Chase Basich <chasebasich@gmail.com>
2015-01-23 21:11:54 -0800

    Created a blank board. Updated the .gitignore file

---
+++ Unity_Files/Assets/Tile Prefabs/GetTileSize.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetTileSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(GetComponent<MeshFilter>().mesh.bounds.size.x);
+		Debug.Log(GetComponent<MeshFilter>().mesh.bounds.size.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//System.Console.WriteLine (GetComponent<MeshFilter>().mesh.bounds);
+	}
+}

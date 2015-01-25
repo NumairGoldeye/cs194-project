16c1a097a5e84a0a0ce2ca1720a6ae4c96d4c0ec
Charlie Yang <charlesy@stanford.edu>
2015-01-24 17:01:38 -0800

    Added public ResourceType
    If you want to use a resource, just use ResourceType.sheep or something
    like that. It should work everywhere because it is a public enum

--- Unity_Files/Assets/_Scripts/Player.cs
+++ Unity_Files/Assets/_Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
-
+using System.Collections.Generic;
 
 
 /*
@@ -10,20 +11,75 @@ between the players.
 
 Also manages some other public members
 
+
+
+
+
+
+
 */
 public class Player : MonoBehaviour {
 
+	public static int playerCount{
+		get; set;
+	}
+
+	public static Player tempPlayer = null;
+
+	public static void CreatePlayer(string name, Color color){
+		// adds a player logic
+	}
+
+
+	public static Player GetPlayerForTurn(int turnNum){
+		return null;
+	}
 
 
 
+	
+	public string playerName; 
+	public int playerId;
+	public Color playerColor;
+
+	public ResourceType[] resources;
+
+	// resources
+	// dev cards 
 
 	// Use this for initialization
 	void Start () {
-	
+		playerCount = 0;
+
+		LogResources();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+
+
+
+
+
+	// Call if you want to add a resource to a player name
+	public bool AddResource(ResourceType resource){
+		return true;
+	}
+
+	void LogResources(){
+		foreach(string resName in Enum.GetNames(typeof(ResourceType))){
+			Debug.Log(resName + " woooo");
+		}
+	}
+
+
+
+
+
+
+
 }
---
+++ Unity_Files/Assets/_Scripts/ResourceClass.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+
+ResourceType is a public enum that we should use to standardize
+the resources we call everywhere
+
+
+Usage:
+
+--
+
+ResourceType tileResource = TileClass.getMaterial();
+Player p1 = Player.getPlayerById(1);
+p1.addResource( tileResource );
+--
+
+-- 
+// Prints each enum by name
+foreach(string resName in Enum.GetNames(typeof(ResourceType))){
+	Debug.Log(resName);	
+}
+--
+
+
+*/
+
+public enum ResourceType {sheep, wood, brick, ore, wheat, desert};
+
+public class ResourceClass {
+
+}

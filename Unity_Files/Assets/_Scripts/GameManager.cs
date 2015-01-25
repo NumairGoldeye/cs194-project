c34583f550df0ae721081b047b051d47cce3edb2
NumairGoldeye <khalilgriffin@gmail.com>
2015-01-24 17:09:51 -0800

    Added numbers to tiles
    Each tile now has a number associated with it.  Does not yet display
    them, and does not handle the desert correctly right now.

--- Unity_Files/Assets/_Scripts/GameManager.cs
+++ Unity_Files/Assets/_Scripts/GameManager.cs
@@ -8,17 +8,31 @@ using System.Collections.Generic;
  * */
 public class GameManager : MonoBehaviour {
 
+	public struct TileInfo {
+		public int diceNumber;
+		public ResourceType type; 
+
+		public TileInfo(ResourceType type, int diceNumber) {
+			this.diceNumber = diceNumber;
+			this.type = type;
+		}
+	}
+
 	private static int numTiles = 19; //number of tiles in play
 	private int tilesRemaining; 
-	public enum tileType {sheep, wood, brick, ore, wheat, desert};
+
 	private static int[] tileCounts = {4, 4, 3, 3, 4, 1};
-	List<tileType> tiles;
+	// The dice numbers for each tyle, in order
+	private static int[] diceNumbers =
+		{5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11, 0};
+
+	List<ResourceType> tiles;
 
 	private void createTiles () {
-		tiles = new List<tileType>();
+		tiles = new List<ResourceType>();
 		tilesRemaining = numTiles;
 		
-		foreach(tileType tile in Enum.GetValues(typeof(tileType))) {
+		foreach(ResourceType tile in Enum.GetValues(typeof(ResourceType))) {
 			for (int i = 0; i < tileCounts[(int)tile]; i++) {
 				tiles.Add(tile);
 			}
@@ -31,17 +45,18 @@ public class GameManager : MonoBehaviour {
 		createTiles ();
 	}
 
-	public tileType assignTileType () {
+	public TileInfo assignTileInfo () {
 		if (tilesRemaining <= 0) {
 			Debug.LogError("Tried to assign tiles past tile limit");
-			return tileType.sheep; //returns sheep to not crash program
+			return new TileInfo(ResourceType.sheep, 2); //returns sheep to not crash program
 		}
 
-		tileType val = tiles[tilesRemaining - 1];
-
+		// TODO(khalilsg): Currently does not handle desert's tyle number correctly.
+		ResourceType val = tiles[tilesRemaining - 1];
+		int diceNum = diceNumbers [tilesRemaining - 1];
 		tilesRemaining--;
 
-		return val;
+		return new TileInfo(val, diceNum);
 	}
 
 	public bool validBuild() {
--- Unity_Files/Assets/_Scripts/TileClass.cs
+++ Unity_Files/Assets/_Scripts/TileClass.cs
@@ -2,35 +2,38 @@
 using System.Collections;
 
 public class TileClass : MonoBehaviour {
-	public enum tileType {sheep, wood, brick, ore, wheat, desert};
-	tileType type;
 
-	private void getType () {
+	ResourceType type;
+	int diceValue;
+
+	private void getInfo () {
 		GameObject go = GameObject.Find ("GameManager");
 		GameManager gm = (GameManager)go.GetComponent (typeof(GameManager));
-		type = (tileType)gm.assignTileType ();
-		Debug.Log (type);
+		GameManager.TileInfo info = gm.assignTileInfo ();
+		type = info.type;
+		diceValue = info.diceNumber;
+		Debug.Log ("Type:" + type + " DiceNumber:" + diceValue);
 	}
 
 	private void getMaterial () {
 
 		switch (type) {
-			case tileType.sheep:
+			case ResourceType.sheep:
 				renderer.material.color = Color.green;
 				break;
-			case tileType.wheat:
+			case ResourceType.wheat:
 				renderer.material.color = Color.yellow;
 				break;
-			case tileType.ore:
+			case ResourceType.ore:
 				renderer.material.color = Color.gray;
 				break;
-			case tileType.brick:
+			case ResourceType.brick:
 				renderer.material.color = Color.red;
 				break;
-			case tileType.wood:
+			case ResourceType.wood:
 				renderer.material.color = Color.black;
 				break;
-			case tileType.desert:
+			case ResourceType.desert:
 				renderer.material.color = Color.white;
 				break;
 		}
@@ -38,7 +41,7 @@ public class TileClass : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		getType ();	
+		getInfo ();	
 		getMaterial ();	
 	}
 	

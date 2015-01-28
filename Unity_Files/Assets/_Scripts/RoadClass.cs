3c36dca5e3b168c55b19856814c450c4ba68eb05
Chris van Harmelen <chrisvh1@stanford.edu>
2015-01-27 20:43:36 -0800

    Added Road click ability

---
+++ Unity_Files/Assets/_Scripts/RoadClass.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadClass : MonoBehaviour {
+
+	private bool built;
+	private bool visible;
+	public GameObject roads;
+	// Use this for initialization
+	void Start () {
+		built = false;
+		visible = false;
+		makeInvisible();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public bool isBuilt() {
+		return built;
+	}
+
+	public void toggleRoad() {
+		if (!built) {
+			if (!visible)
+				makeVisible();
+			else
+				makeInvisible();
+		}
+	}
+	void makeInvisible() {
+		visible = false;
+		Color temp = renderer.material.color;
+		temp.a = 0;
+		renderer.material.color = temp;		
+	}
+
+	void makeVisible() {
+		visible = true;
+		Color temp = renderer.material.color;
+		temp.a = 0.8f;
+		renderer.material.color = temp;
+	}
+
+	void OnMouseDown() {
+		built = true;
+		Color temp = renderer.material.color;
+		temp.a = 1;
+		renderer.material.color = temp;
+		roads.BroadcastMessage ("toggleRoad");
+//		gameObject.SendMessageUpwards ("toggleChildren");
+	}
+}

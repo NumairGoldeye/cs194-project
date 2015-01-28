d73558870378981273fb41fc7f0802b140ad82da
NumairGoldeye <khalilgriffin@gmail.com>
2015-01-27 23:06:46 -0800

    Added BoardGraph interface
    Designed (with Kevin) the interface for the BoardGraph, and all of its
    exposed classed.  Note: this is just the interface, implementation
    forthcoming.

---
+++ Unity_Files/Assets/_Scripts/PortClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class PortClass {
+	public ResourceType type;
+
+	public PortClass(ResourceType type) {
+		this.type = type;
+	}
+}
+
+
---
+++ Unity_Files/Assets/_Scripts/Vertex.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+/** A vertex is defined by the tiles that surround it, and its city and settlement.*/
+public class Vertex {
+	
+	// Note: Every vertex has a city and settlment assocaiated with it,
+	// but they are not necessarily active.
+	public CityClass city { get; set; }
+	public SettlementClass settlement { get; set; }
+	
+	public List<TileClass> tiles;
+	public List<PortClass> ports;
+
+	public Vertex (List<TileClass> tiles, List<PortClass> ports, CityClass city, SettlementClass settlement) {
+		this.tiles = tiles;
+		this.ports = ports;
+		this.city = city;
+		this.settlement = settlement;
+	}
+
+	public bool hasCity() {
+		// TODO: When city has "isBuilt" method, call it.
+	}
+
+	public bool hasSettlement() {
+		return settlement.isBuilt();
+	}
+}
+

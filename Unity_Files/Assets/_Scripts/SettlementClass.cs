e2c1d0cd7a5faa8ee7fe2d30f592938bb215e171
Chris van Harmelen <chrisvh1@stanford.edu>
2015-01-27 21:14:40 -0800

    Created a few settlements and Made them clickable.
    Also made a button to demonstrate the click ability of the settlements.

---
+++ Unity_Files/Assets/_Scripts/SettlementClass.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettlementClass : MonoBehaviour {
+
+	private bool built;
+	private bool visible;
+	public GameObject settlements;
+	// Use this for initialization
+	void Start () {
+		built = false;
+		visible = false;
+		makeInvisible();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool isBuilt() {
+		return built;
+	}
+
+	private void upgradeToCity() {
+
+	}
+
+	public void toggleSettlements() {
+		if (!built) {
+			if (!visible)
+				makeVisible();
+			else
+				makeInvisible();
+		} else {
+			//Upgrade to city
+			upgradeToCity();
+		}
+	}
+	void makeInvisible() {
+		visible = false;
+		Color temp = renderer.material.color;
+		temp.a = 0;
+		renderer.material.color = temp;		
+	}
+	
+	void makeVisible() {
+		visible = true;
+		Color temp = renderer.material.color;
+		temp.a = 0.8f;
+		renderer.material.color = temp;
+	}
+	
+	void OnMouseDown() {
+		built = true;
+		Color temp = renderer.material.color;
+		temp.a = 1;
+		renderer.material.color = temp;
+		settlements.BroadcastMessage ("toggleSettlements");
+	}
+}

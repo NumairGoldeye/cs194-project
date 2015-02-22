abf17c814f88d4d01af00d07bf6d0e6b4cc0af64
Chase Basich <chasebasich@gmail.com>
2015-02-22 01:10:37 -0800

    Tied ports to cities/settlements

--- Unity_Files/Assets/_Scripts/PortClass.cs
+++ Unity_Files/Assets/_Scripts/PortClass.cs
@@ -1,10 +1,14 @@
+using UnityEngine;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
-public class PortClass {
+public class PortClass : MonoBehaviour {
 	public ResourceType type;
+	public int ratio;
 	
-	public PortClass(ResourceType type) {
+	public PortClass(ResourceType type, int ratio) {
 		this.type = type;
+		this.ratio = ratio;
 	}
 }

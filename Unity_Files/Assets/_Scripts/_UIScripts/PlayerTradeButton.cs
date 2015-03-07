eb3829fbed43acd135893f366e636272d563cb45
NumairGoldeye <khalilgriffin@gmail.com>
2015-03-06 20:26:17 -0800

    Displays proposed trade on confirmation screen

--- Unity_Files/Assets/_Scripts/_UIScripts/PlayerTradeButton.cs
+++ Unity_Files/Assets/_Scripts/_UIScripts/PlayerTradeButton.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Text;
+using System.Collections.Generic;
 
 public class PlayerTradeButton : MonoBehaviour {
 	
 	public TradeConsole tradeConsole;
 	public GameObject tradeConfirm;
+	public Text tradeGiveText;
+	public Text tradeGetText;
+
 	public ComboBox tradePlayerBox;
 	public Button acceptButton;
 
@@ -29,7 +34,23 @@ public class PlayerTradeButton : MonoBehaviour {
 		} else {
 			acceptButton.gameObject.SetActive (false);
 		}
+		tradeGiveText.text = "Give: " + PlayerTradeButton.GetCounterText(turnPlayerToGetCounter);
+		tradeGetText.text = "Get: " + PlayerTradeButton.GetCounterText(turnPlayerToGiveCounter);
 		tradeConfirm.SetActive (true);
+	}
+
+	private static string GetCounterText(TradeCounter counter) {
+		StringBuilder builder = new StringBuilder ();
+		bool isFirst = true;
+		foreach (KeyValuePair<ResourceType, int> pair in counter) {
+			if (isFirst) {
+				isFirst = false;
+			} else {
+				builder.Append(", ");
+			}
+			builder.Append("" + pair.Value + " " + pair.Key.ToString() + " "); 
+		}
+		return builder.ToString();
 	}
 	
 }

ef86322e6d59730cc47be238bc8e2116cc667b83
Charlie Yang <charlesy@stanford.edu>
2015-03-14 15:58:37 -0700

    adding UI stuff
    sync text to button so disabled buttons are more obvious

---
+++ Unity_Files/Assets/_Scripts/_UIScripts/ButtonTextOpacitySync.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+/// <summary>
+/// Sync's the text ofa  button to have the opacity of its button parent
+/// </summary>
+public class ButtonTextOpacitySync : MonoBehaviour {
+
+	Button btn;
+	Text txt;
+
+	// Use this for initialization
+	void Start () {
+		txt = GetComponent<Text>();
+		btn = transform.parent.gameObject.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		float opacity = btn.
+	}
+}

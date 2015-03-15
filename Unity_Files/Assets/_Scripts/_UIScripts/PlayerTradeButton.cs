50bf0694dbe9f9c0d418c81b75a326db7b49949b
NumairGoldeye <khalilgriffin@gmail.com>
2015-03-14 20:02:40 -0700

    Refactored TradeConfirm

---
+++ Unity_Files/Assets/_Scripts/_UIScripts/PlayerTradeButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections.Generic;
+
+public class PlayerTradeButton : MonoBehaviour {
+	
+	public TradeConfirm tradeConfirm;
+
+	Button btn;
+
+	// Use this for initialization
+	void Start () {
+		btn = gameObject.GetComponent<Button>();
+		btn.onClick.AddListener(tradeConfirm.Display);
+	}
+}

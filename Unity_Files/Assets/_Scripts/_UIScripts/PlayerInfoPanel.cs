f99d23eb361a102b9145e42dac0a8632201b5f14
Charlie Yang <charlesy@stanford.edu>
2015-03-17 16:02:19 -0700

    re-added the personal ui bits

--- Unity_Files/Assets/_Scripts/_UIScripts/PlayerInfoPanel.cs
+++ Unity_Files/Assets/_Scripts/_UIScripts/PlayerInfoPanel.cs
@@ -1,67 +1,65 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 
 
 /// <summary>
-/// Leader board player name.
+/// Player info panel UI on your own UI
+/// 
+/// Very similar to LeaderBoardPlayerName
 /// </summary>
-public class LeaderBoardPlayerName : MonoBehaviour {
+public class PlayerInfoPanel : MonoBehaviour {
 
-	Player player; // set by LeaderBoardPanel;
-	bool set = false;
+	Player player;
 
-	// Set in inspector
 	public Text playerName;
 	public Text knightCount;
-	public Text cardCount;
-
+	public Text victoryCount;
+	
 	public Image largestArmyIcon;
 	public Image longestRoadIcon;
 
-
-
+	bool setup = false;
 
 	// Use this for initialization
 	void Start () {
-		// Why does the UI autoscale it? so annoying, sigh
-		transform.localScale = new Vector3(1f,1f,1f);
-
+		player = GameManager.Instance.myPlayer;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		Debugger.Log ("LeaderBoard", "WHEE");
-
-		if (set){
-//
-//
-//			Debugger.Log ("LeaderBoard", player.playerName);
-
+			
+//		if (GameManager.Instance.myPlayer != null){
+			player = GameManager.Instance.myPlayer;
+			setup = true;
+			
+//		}
+			//
+		//
+		try {
+			Debugger.Log ("LeaderBoard", player.playerName);
+			
 			playerName.text = player.playerName;
 			knightCount.text = player.numUsedKnights.ToString();
-			cardCount.text = player.GetTotalHandSize().ToString();
-
+			victoryCount.text = player.victoryPoints.ToString();
+			
 			if (player.hasLargestArmy){
 				largestArmyIcon.gameObject.SetActive(true);
 			} else  {
 				largestArmyIcon.gameObject.SetActive(false);
 			}
-
+			
 			if (player.hasLongestRoad){
 				longestRoadIcon.gameObject.SetActive(true);
 			} else {
 				longestRoadIcon.gameObject.SetActive(false);
-//				longestRoadIcon.color = new Color(1f,1f,1f);
+				//				longestRoadIcon.color = new Color(1f,1f,1f);
 			}
-
-
+		} catch ( NullReferenceException exception) {
+				//Do other stuff.
 		}
-	}
-
-	public void SetPlayer( Player p){
-		set = true; // because (player != null) just won't cut it
-		Debugger.Log ("LeaderBoard", p.playerName);
-		player = p;
+			
+			
 	}
 }

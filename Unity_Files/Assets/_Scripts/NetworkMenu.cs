cc15859bdf06e1d05c6abf4220d8aa26f784fd86
Chris van Harmelen <chrisvh1@stanford.edu>
2015-03-17 19:10:35 -0700

    Commented onGUI in network menu out

--- Unity_Files/Assets/_Scripts/NetworkMenu.cs
+++ Unity_Files/Assets/_Scripts/NetworkMenu.cs
@@ -146,55 +146,55 @@ public class NetworkMenu : MonoBehaviour {
 	}
 	
 	
-	private void OnGUI()
-	{
-		if (!connected) {
-			//We should rework this GUI so that there are two buttons: Host & Connect
-			// Clicking host will prompt a host name, and connect will display a list of hosted games
-			GUILayout.Label("Game Name");
-			gameName = GUILayout.TextField(gameName);
-			GUILayout.Label("Player Name");
-			GameManager.Instance.myPlayerName = GUILayout.TextField(GameManager.Instance.myPlayerName);
-
-			if (GUILayout.Button ("Host")) {
-				if (gameName.Equals("") || GameManager.Instance.myPlayerName.Equals("")) return;
-				NetworkConnectionError error = Network.InitializeServer(1000, 6832 ,true);
-			}
-			if (GUILayout.Button("Connect")) {
-				if (GameManager.Instance.myPlayerName.Equals("")) return;
-				MasterServer.RequestHostList(gameType);
-			}
-		} else if (!GameManager.Instance.gameStarted) { 
-			if (Network.isServer) {
-				GUILayout.Label("Running as a server");
-				if (GUILayout.Button("Start")) {
-
-					//KEEP These Actions Need to happen when the host starts the game
-
-//					int startingPlayerID = Random.Range(0, GameManager.Instance.players.Count);
-					int startingPlayerID = 0;
-					GameManager.Instance.networkView.RPC("syncCurrentPlayer", RPCMode.All, startingPlayerID);
-					GameManager.Instance.createTiles();
-					GameManager.Instance.syncStartStateWithClients();
-					GameManager.Instance.gameStarted = true;
-					GameManager.Instance.networkView.RPC("startupGame", RPCMode.All);
-//					GameManager.Instance.graph.getSettlement(4).buildSettlement();
-//					GameManager.Instance.myPlayer.AddResource(ResourceType.Brick, 1);
-//					GameManager.Instance.myPlayer.AddResource(ResourceType.Sheep, 1);
-//					GameManager.Instance.myPlayer.AddResource(ResourceType.Wheat, 1);
-//					GameManager.Instance.myPlayer.AddResource(ResourceType.Wood, 1);
-					Debugger.Log("PlayerHand", "Player has: " + GameManager.Instance.myPlayer.totalResources.ToString() + " resources");
-
-//					Debugger.Log ("PlayerHand", "Players in game: " + GameManager.Instance.players.Count.ToString ());
-				}
-			}
-			GUILayout.Label("Name: " + GameManager.Instance.players.Find(x => x.playerId == GameManager.Instance.myPlayer.playerId).playerName);
-			GUILayout.Label("Connections: " + Network.connections.Length.ToString());
-		} else if (GameManager.Instance.gameStarted) {
-			Player player = GameManager.Instance.players.Find(x => x.playerId == GameManager.Instance.myPlayer.playerId);
-			GUILayout.Label("Name: " + player.playerName + ", ID: " + player.playerId);
-		}
-	}
+//	private void OnGUI()
+//	{
+//		if (!connected) {
+//			//We should rework this GUI so that there are two buttons: Host & Connect
+//			// Clicking host will prompt a host name, and connect will display a list of hosted games
+//			GUILayout.Label("Game Name");
+//			gameName = GUILayout.TextField(gameName);
+//			GUILayout.Label("Player Name");
+//			GameManager.Instance.myPlayerName = GUILayout.TextField(GameManager.Instance.myPlayerName);
+//
+//			if (GUILayout.Button ("Host")) {
+//				if (gameName.Equals("") || GameManager.Instance.myPlayerName.Equals("")) return;
+//				NetworkConnectionError error = Network.InitializeServer(1000, 6832 ,true);
+//			}
+//			if (GUILayout.Button("Connect")) {
+//				if (GameManager.Instance.myPlayerName.Equals("")) return;
+//				MasterServer.RequestHostList(gameType);
+//			}
+//		} else if (!GameManager.Instance.gameStarted) { 
+//			if (Network.isServer) {
+//				GUILayout.Label("Running as a server");
+//				if (GUILayout.Button("Start")) {
+//
+//					//KEEP These Actions Need to happen when the host starts the game
+//
+////					int startingPlayerID = Random.Range(0, GameManager.Instance.players.Count);
+//					int startingPlayerID = 0;
+//					GameManager.Instance.networkView.RPC("syncCurrentPlayer", RPCMode.All, startingPlayerID);
+//					GameManager.Instance.createTiles();
+//					GameManager.Instance.syncStartStateWithClients();
+//					GameManager.Instance.gameStarted = true;
+//					GameManager.Instance.networkView.RPC("startupGame", RPCMode.All);
+////					GameManager.Instance.graph.getSettlement(4).buildSettlement();
+////					GameManager.Instance.myPlayer.AddResource(ResourceType.Brick, 1);
+////					GameManager.Instance.myPlayer.AddResource(ResourceType.Sheep, 1);
+////					GameManager.Instance.myPlayer.AddResource(ResourceType.Wheat, 1);
+////					GameManager.Instance.myPlayer.AddResource(ResourceType.Wood, 1);
+//					Debugger.Log("PlayerHand", "Player has: " + GameManager.Instance.myPlayer.totalResources.ToString() + " resources");
+//
+////					Debugger.Log ("PlayerHand", "Players in game: " + GameManager.Instance.players.Count.ToString ());
+//				}
+//			}
+//			GUILayout.Label("Name: " + GameManager.Instance.players.Find(x => x.playerId == GameManager.Instance.myPlayer.playerId).playerName);
+//			GUILayout.Label("Connections: " + Network.connections.Length.ToString());
+//		} else if (GameManager.Instance.gameStarted) {
+//			Player player = GameManager.Instance.players.Find(x => x.playerId == GameManager.Instance.myPlayer.playerId);
+//			GUILayout.Label("Name: " + player.playerName + ", ID: " + player.playerId);
+//		}
+//	}
 
 
 }
